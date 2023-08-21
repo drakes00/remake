fbfb63cf8f4f46eb6c828af3820c4bb02e67e3bf
Maxime Puys <drakes00@gmail.com>
2023-08-21 22:26:09 +0200

    Upadted: ReMakeFiles import default builders

--- remake/__init__.py
+++ remake/__init__.py
@@ -5,4 +5,3 @@ from remake.main import Target, Builder, Rule, PatternRule
 from remake.main import findBuildPath, executeReMakeFileFromDirectory, generateDependencyList, buildDeps
 from remake.main import setDryRun, setVerbose, setDevTest, unsetDryRun, unsetVerbose, unsetDevTest
 from remake.context import getCurrentContext, getOldContext
-#from remake.builders import html2pdf_chrome, md2html, jinja2, pdfcrop
