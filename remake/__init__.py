9f374ee9c2d7be146bf3c01bcc39fa3ef05d9e3d
Maxime Puys <drakes00@gmail.com>
2023-10-24 22:32:02 +0200

    Released: v1.0.0

--- remake/__init__.py
+++ remake/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = "1.0.0"
+
 from remake.main import AddTarget, Builder, Rule, PatternRule, VirtualDep, VirtualTarget, AddVirtualTarget
 from remake.main import findBuildPath, executeReMakeFileFromDirectory, generateDependencyList, buildDeps, cleanDeps
 from remake.main import setDryRun, setVerbose, setDevTest, unsetDryRun, unsetVerbose, unsetDevTest
