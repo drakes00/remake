d2ae0bfea17c6866dbd54569f4e8b395f9c1bde9
Maxime Puys <drakes00@gmail.com>
2024-02-26 18:05:24 +0100

    Bump: v1.1.0

--- remake/__init__.py
+++ remake/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "v1.1.0+0.2d075ba"
+__version__ = "v1.1.0+0.9cbe564"
 
 from remake.main import AddTarget, Builder, Rule, PatternRule, VirtualDep, VirtualTarget, AddVirtualTarget, GlobPattern
 from remake.main import findBuildPath, executeReMakeFileFromDirectory, generateDependencyList, buildDeps, cleanDeps
